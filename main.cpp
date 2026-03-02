8ec0238877882d024352b57b6c6ad1ed35de73b7
IvanObukhod <IVAN.OBYKHOD@lnu.edu.ua>
2026-03-02 17:47:50 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -4,5 +4,5 @@
 using namespace std;
 
 int main(){
-    calcGeneralPrice("./input.txt");
+    calcGeneralPrice("input.txt");
 }
--- money.cpp
+++ money.cpp
@@ -12,25 +12,29 @@ void fixCops(Money *money){
 void addMoney(Money *money, Money *addMoney){
     money->grn += addMoney->grn;
     money->cop += addMoney->cop;
-    fixCops(money);
+    /*fixCops(money);*/
 }
 
 void multiplyMoney(Money *money, int count){
     money->grn *= count;
     money->cop *= count;
-    fixCops(money);
+    /*fixCops(money);*/
 }
 
 void roundMoney(Money *money){
-    money->cop = money->cop / 10 * 10 + (money->cop % 10 >= 8 ? 10 : 0);
-    fixCops(money);
+    money->cop = money->cop / 10 * 10 + (money->cop % 10 >= 4 ? 10 : 0);
 }
 
 void printMoney(Money *money){
-    std::cout << money->grn << "grn " << money->cop << "cop\n"; 
+    cout << "before round: " << money->grn << "grn " << money->cop << "cop\n"; 
 }
 
-void calcGeneralPrice(char *path){
+void printMoney2(Money *money){
+    cout << "after round: " << money->grn << "grn " << money->cop << "cop\n"; 
+}
+
+
+void calcGeneralPrice(const char *path){
     FILE *file;
     int err = fopen_s(&file, path, "r");
     
@@ -43,7 +47,7 @@ void calcGeneralPrice(char *path){
         char product[256];
         while (fgets(buffer, sizeof(buffer), file)) {
             if (sscanf(buffer, "%s %u %hu %u", product, &grn, &cop, &count) == 4) {
-                if(count <= 0 || grn < 0 || cop < 0 || cop >= 100){
+                if(count < 0 || grn < 0 || cop < 0){
                     cout << "incorrect format of input\n";
                     return;
                 }
@@ -56,8 +60,14 @@ void calcGeneralPrice(char *path){
                 return;
             }
         }
-        roundMoney(&money);
+        
+        if(money.cop >= 100){
+            fixCops(&money);
+        }
+        
         printMoney(&money);
+        roundMoney(&money);
+        printMoney2(&money);
         fclose(file);
     }
     else{
